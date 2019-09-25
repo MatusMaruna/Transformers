717f1c61dc935a9d724a2030f70141689d0c0592
matusmaruna <matus.maruna@gmail.com>
2019-09-25 11:50:03 +0200

    update

--- 4DV507.rd222dv.A2/src/Mylistener.java
+++ 4DV507.rd222dv.A2/src/Mylistener.java
@@ -5,29 +5,33 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Mylistener implements ParseTreeListener {
         @Override public void enterEveryRule(ParserRuleContext ctx) {  
-            System.out.println("rule entered: " + ruleNames[ctx.getRuleIndex()]);    
+            System.out.println("rule entered: " + resolveName(ctx));    
         }
 
 		@Override
-		public void exitEveryRule(ParserRuleContext arg0) {
-			System.out.println("rule exited: " +  arg0.getText());
+		public void exitEveryRule(ParserRuleContext ctx) {
+			//System.out.println("rule exited: " +  arg0.getText());
 		}
 
 		@Override
-		public void visitErrorNode(ErrorNode arg0) {
+		public void visitErrorNode(ErrorNode ctx) {
 			//System.err.println("Visit Error: " + arg0.getText());
 			
 		}
 
 		@Override
-		public void visitTerminal(TerminalNode arg0) {
-			System.out.println("Terminal: " + arg0.getText());
+		public void visitTerminal(TerminalNode ctx) {
+		//	System.out.println("Terminal: " + arg0.getText());
 			
 		}
 		
 		String[] ruleNames;
 		public void loadParser(OfpParser parser) {  
 		    ruleNames = parser.getRuleNames(); 
+		}
+		
+		public String resolveName(ParserRuleContext ctx) {
+			return ruleNames[ctx.getRuleIndex()];
 		}
     }
 
