41a1fbc182d6721ea5eb288950f25158cb79add5
matusmaruna <matus.maruna@gmail.com>
2019-09-30 10:30:41 +0200

    test

--- 4DV507.rd222dv.A2/src/OfpType.java
+++ 4DV507.rd222dv.A2/src/OfpType.java
@@ -1,29 +1,15 @@
 
-public class OfpType {
+public enum OfpType {
+	Int, 
+	String, 
+	IntArray,
+	Char, 
+	Float,
+	Bool, 
+	FloatArray, 
+	CharArray, 
+	StringArray, 
+	
 
-	public static final OfpType intType = new OfpType("int");
-	public static final OfpType intArrType = new OfpType("int[]");
-
-	public static final OfpType argsType = new OfpType("String[]");
-
-	public static OfpType getTypeFor(String typeName) {
-		return null;
-
-	}
-
-	private final String name;
-
-	private OfpType(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String toString() {
-		return name;
-	}
 
 }
